420ef8227dd585ff368d3b4461e8b6cd4f608770
Oriol Alfonso <oriol.alfonso@aetion.com>
2024-12-19 15:45:07 +0100

    Implement definitionProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,60 @@
+import * as fs from 'fs';
+
 import * as vscode from 'vscode';
+
 
 export class Provider {
   context: vscode.ExtensionContext
-
-  range = new vscode.Range(0, 0, 0, 0)
 
   constructor(context: vscode.ExtensionContext) {
     this.context = context
   }
 
   provideDefinition(document: vscode.TextDocument, position: vscode.Position) {
-    const URI = document.uri
-    const currentFolder = vscode.Uri.joinPath(URI, '../')
-    const importLine = document.lineAt(position)
-
-    console.log('importLine', importLine)
-    vscode.window.showInformationMessage("another thing")
     const start = performance.now()
-    // const loc = vscode.Location()  // TODO: fill with the correct location
-  
-    console.log('Time: ', performance.now() - start)
+    const importLine = document.lineAt(position)
+    if (importLine.text.includes("call: ")) {
+		  const flowName = document.getText(document.getWordRangeAtPosition(position)).split(/\r?\n/)[0];
+      const flowFlag = flowName + ":"
+      console.log("Flow name: ", flowName)
+
+      let rootPath: string | undefined
+      if (vscode.workspace.workspaceFolders !== undefined) {
+        rootPath = vscode.workspace.workspaceFolders[0].uri.path
+      } else {
+        vscode.window.showErrorMessage("CONCORD-GOTO: Working folder not found, open a folder an try again")
+        return undefined
+      }
+
+      let locations: Array<vscode.Location> = [];
+      try{
+        const filesInDirectory = fs.readdirSync(rootPath, {
+          encoding: "utf-8",
+          recursive: true
+        })
+        for (const file of filesInDirectory) {
+          const absolute = rootPath + "/" + file
+          if (!fs.statSync(absolute).isDirectory() && (absolute.endsWith(".concord.yaml") || absolute.endsWith(".concord.yml"))) {
+            // TODO: use async -> import { readFile } from 'fs/promises';
+            const fileContent: string[] = fs.readFileSync(absolute, 'utf-8').split(/\r?\n|\r|\n/g)
+            for (let lineIndex: number = 0; lineIndex < fileContent.length; lineIndex++) {
+              const lineContent: string = fileContent[lineIndex]
+              const charIndex: number = lineContent.indexOf(flowFlag)
+              if (charIndex >= 0 && lineContent.trim() === flowFlag) {
+                const uri = vscode.Uri.parse(absolute)
+                const range = new vscode.Range(lineIndex, charIndex, lineIndex, charIndex + flowFlag.length)
+                const location = new vscode.Location(uri, range)
+                locations.push(location)
+              }
+            }
+          }
+        }
+      } catch(err){
+          console.error(err);
+      }
+      console.log('Time: ', performance.now() - start)
+      return locations
+    }
 
     return undefined
   }
@@ -32,7 +67,7 @@ export function activate(context: vscode.ExtensionContext) {
     scheme: 'file',
     language: 'yaml'
   }
-  console.log("hei")
+
   const provider = new Provider(context)
 
   const definitionProvider = vscode.languages.registerDefinitionProvider(
@@ -40,10 +75,9 @@ export function activate(context: vscode.ExtensionContext) {
     provider
   )
 
-  vscode.window.showInformationMessage('Not funny');
+  vscode.window.showInformationMessage('Concord-goto initialized');
 
   context.subscriptions.push(definitionProvider)
-
 }
 
 export function deactivate() {}
